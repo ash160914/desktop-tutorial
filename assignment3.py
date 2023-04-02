433924011e0e7c3ee9c1e9ecc17e808cd8cdf4c4
ash160914 <118343165+ash160914@users.noreply.github.com>
2023-04-02 16:42:47 +0530

    function program

---
+++ assignment3.py
@@ -0,0 +1,10 @@
+# function
+
+total=0
+ele=0
+list1 = [11,5,17,18,23]
+while(ele < len(list1)):
+        total=total + list1[ele]
+        ele += 1
+print("sum  of all elements in given list: ",total)
+    
